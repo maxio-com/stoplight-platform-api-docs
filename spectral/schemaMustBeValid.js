a8e71e7afaca88bc5632e0b08a8383a6aa08db39
Lukasz Ziemnicki <lukasz.ziemnicki@o2.pl>
2023-09-01 13:29:57 +0200

    merge with main

---
+++ spectral/schemaMustBeValid.js
@@ -0,0 +1,22 @@
+export default (schema, _, context) => {
+  if (schema.oneOf || schema.anyOf || schema.allOf) return;
+
+  if (
+    schema.type === "object" &&
+    (!schema.properties || Object.keys(schema.properties).length === 0)
+  ) {
+    return [
+      {
+        message: "Schema for type 'object' must define valid 'properties'",
+      },
+    ];
+  }
+
+  if (!schema.type && !schema.properties && !schema["$ref"]) {
+    return [
+      {
+        message: "Schema must use '$ref' or 'type' with 'properties'",
+      },
+    ];
+  }
+};
