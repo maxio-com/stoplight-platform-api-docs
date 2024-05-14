519901aa8f5efae079b59847dd94ed0ec189265b
maciej-nedza <76946708+maciej-nedza@users.noreply.github.com>
2024-05-14 10:10:28 +0200

    Restore create offer step in product walkthrough (#507)

--- portal/static/guided-walkthrough-scripts/ProductCatalog.js
+++ portal/static/guided-walkthrough-scripts/ProductCatalog.js
@@ -201,5 +201,48 @@ In your app or business, you might call these Products your "Plans" or "Feature 
         });
       },
     },
+    "Step 5": {
+      name: "Create Offer",
+      stepCallback: async (stepState) => {
+        const stateAfterProductCreation = stepState?.["Step 2"];
+        const stateAfterComponentCreation = stepState?.["Step 3"];
+        const stateAfterCouponCreation = stepState?.["Step 4"];
+        await portal.setConfig((defaultConfig) => ({
+          ...defaultConfig,
+        }));
+        return workflowCtx.showEndpoint({
+          description:
+            "This endpoint is used to create an offer within your Chargify site by sending a POST request.",
+          endpointPermalink: "$e/Offers/createOffer",
+          args: {
+            body: {
+              offer: {
+                name: "Solo",
+                handle: "han_shot_first",
+                description: "A Star Wars Story",
+                product_id: stateAfterProductCreation?.data?.["product"]?.id,
+                components: [
+                  {
+                    component_id:
+                      stateAfterComponentCreation?.data?.["component"]?.id,
+                    starting_quantity: 1,
+                  },
+                ],
+                coupons: [stateAfterCouponCreation?.data?.["coupon"]?.code],
+              },
+            },
+          },
+          verify: (response, setError) => {
+            if (response.StatusCode === 201) {
+              return true;
+            }
+            setError(
+              "API Call wasn't able to get a valid response. Please try again.",
+            );
+            return false;
+          },
+        });
+      },
+    },
   };
 }
