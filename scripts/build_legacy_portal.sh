c2b614e0ea3f442516ef0433c303eb4811cd39bf
Maciej Nedza <maciej.nedza@keen.io>
2024-06-14 11:28:08 +0200

    [DE-842] Prefix guided walkthroughs with base url

--- scripts/build_legacy_portal.sh
+++ scripts/build_legacy_portal.sh
@@ -29,7 +29,7 @@ if [ "$BASE_URL" = "" ]; then
   BASE_URL="http://localhost:8080/legacy"
 fi
 echo "Overriding base url with $BASE_URL" | tee -a "$GITHUB_STEP_SUMMARY"
-sed -i '.backup' -e "s,\"baseUrl\": \"\",\"baseUrl\": \"$BASE_URL\",g" ./build/tmp/portal/APIMATIC-BUILD.json
+sed -i '.backup' -e "s,--BASE-URL-PLACEHOLDER--,$BASE_URL,g" ./build/tmp/portal/APIMATIC-BUILD.json
 
 echo "Overriding new portal url with $new_portal_url" | tee -a "$GITHUB_STEP_SUMMARY"
 sed -i '.backup' -e "s,--URL_PLACEHOLDER--,$new_portal_url,g" ./build/tmp/portal/spec/openapi.yaml
