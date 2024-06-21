2c647b9e8f4da20dc9ee4a1e74fc6149be10dc14
Maciej Nedza <maciej.nedza@keen.io>
2024-06-21 10:05:19 +0200

    test

--- scripts/prepare_production_portal.sh
+++ scripts/prepare_production_portal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-awk '/<\/head>/ && !f {system("cat ./scripts/google_analytics_tag_head.txt"); f=1} 1' ./build/static-portal/index.html > tmp_index.html && mv tmp_index.html ./build/static-portal/index.html
+awk '/<\/head>/ && !f {system("cat ./scripts/search.txt"); f=1} 1' ./build/static-portal/index.html > tmp_index.html && mv tmp_index.html ./build/static-portal/index.html
 awk '/<body>/ && !f {print; system("cat ./scripts/google_analytics_tag_body.txt"); f=1; next} 1' ./build/static-portal/index.html > tmp_index.html && mv tmp_index.html ./build/static-portal/index.html
